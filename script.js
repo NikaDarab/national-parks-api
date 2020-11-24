7e8b6938c6af4e4e463c63bd25f9f81944c1cff4
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2020-11-24 10:06:19 -0600

    multiple inputs

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-function getPark(state,limit) {
-  let stateUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${limit}&api_key=LRg7FeFjjKkaKqbFmTUvnCdBVPf9AxTvxDkmu6w0`
-  fetch(stateUrl).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  })
-  .then(responseJson => displayResults(responseJson))
-  .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    for (let i=0; i< responseJson.data.length;i++)
-    {
-      $('#result-list').append (`<li><h3>${responseJson.data[i]}</h3></li>`)
+function getPark(state, limit) {
+  state = state.split(",");
+  let stateUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${limit}&api_key=LRg7FeFjjKkaKqbFmTUvnCdBVPf9AxTvxDkmu6w0`;
+  fetch(stateUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
       }
-    
-  });
-};
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+      for (let i = 0; i < responseJson.data.length; i++) {
+        $("#result-list").append(`<li><h3>${responseJson.data[i]}</h3></li>`);
+      }
+    });
+}
 
 function displayResults(responseJson) {
   // clear previous results if any
-  $('#results-list').empty();
+  $("#results-list").empty();
 
   //iterate through the items
-  for ( let i=0; i< responseJson.data.length;i++){
-    $('#results-list').append (
+  for (let i = 0; i < responseJson.data.length; i++) {
+    $("#results-list").append(
       `<li>
         <h3>${responseJson.data[i].fullName}</h3>
         <p>${responseJson.data[i].description}</p>
@@ -32,17 +32,17 @@ function displayResults(responseJson) {
         </p>
         <a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a>
       </li>`
-    )
+    );
   }
-};
+}
 
-function watchForm(state,limit) {
-  $('#js-form').submit(event => {
+function watchForm(state, limit) {
+  $("#js-form").submit((event) => {
     event.preventDefault();
-    state = $('#js-search-state').val();
-    limit = $('#js-max-results').val();
+    state = $("#js-search-state").val();
+    limit = $("#js-max-results").val();
     getPark(state, limit);
   });
-};
+}
 
 $(watchForm);
